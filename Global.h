28dc751539d795f68a2a43318a566798dd5150d5
Tyldar <tyldar@hotmail.com>
2016-11-12 01:26:39 +0100

    Fixed compilation under linux

--- Global.h
+++ Global.h
@@ -3,8 +3,8 @@
 // Required by module
 #include	"SDK.h"
 #include	"ScriptFunctions.h"
-#include	"MySQL\handler.h"
-#include	"MySQL\result.h"
+#include	"MySQL/handler.h"
+#include	"MySQL/result.h"
 #include	<string>
 #include	<stdlib.h>
 #include	<list>
